3ea3a65135f7ad6fd417a5edd12fc3db8c809742
Miia <miiaheiskan@gmail.com>
2022-04-28 11:26:01 +0300

    ex5.4

---
+++ week5/laskin/src/operaatiot.py
@@ -0,0 +1,30 @@
+class Summa:
+    def __init__(self, sovelluslogiikka):
+        self._sovelluslogiikka = sovelluslogiikka
+        
+    def suorita(self, luku):
+        self._sovelluslogiikka.plus(luku)
+        return self._sovelluslogiikka.tulos
+
+class Erotus:
+    def __init__(self, sovelluslogiikka):
+        self._sovelluslogiikka = sovelluslogiikka
+        
+    def suorita(self, luku):
+        self._sovelluslogiikka.miinus(luku)
+        return self._sovelluslogiikka.tulos
+
+class Nollaus:
+    def __init__(self, sovelluslogiikka):
+        self._sovelluslogiikka = sovelluslogiikka
+        
+    def suorita(self):
+        self._sovelluslogiikka.nollaa()
+        return self._sovelluslogiikka.tulos
+
+class Kumoa:
+    def __init__(self, sovelluslogiikka):
+        self._sovelluslogiikka = sovelluslogiikka
+        
+    def suorita(self):
+        return self._sovelluslogiikka.tulos
